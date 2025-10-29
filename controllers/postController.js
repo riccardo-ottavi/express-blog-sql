e3b79cca69556a9df5614c3c2a1f2098ad48747d
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-29 14:33:40 +0100

    inserisci show()

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,25 +12,13 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    const id = parseInt(req.params.id)
-
-    // cerchiamo il post tramite id
-    const post = postsIndex.find(post => post.id === id);
-
-    // Facciamo il controllo
-    if (!post) {
-
-        //Imposto lo status 404
-        res.status(404)
-
-        // Restituisco un JSON con le altre informazioni
-        return res.json({
-            error: "Not Found",
-            message: "post non trovato"
-        })
-    }
-    // Restituiamolo sotto forma di JSON   
-    res.json(post);
+    const id = req.params.id
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+    connection.query(sql, [id], (err, results) => {
+        if(err) return res.status(500).json({error: 'Database query failed'})
+        if(results.length === 0) return res.status(404).json({error: 'Post not found'})
+        res.json(results[0]);
+    });
 }
 
 function store(req, res) {
